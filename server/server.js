89409369eebd65a9f5ab26aee13286d7921a8091
Niraj1232005 <rathodniraj.com@gmail.com>
2026-04-17 22:03:59 +0530

    deployment ready

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+require("dotenv").config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -12,8 +14,10 @@ app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/reviews", require("./routes/reviewRoutes"));
 
 // MongoDB connect
-mongoose.connect("mongodb+srv://admin:admin123@cluster0.7ksyseo.mongodb.net/perfumeDB")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
